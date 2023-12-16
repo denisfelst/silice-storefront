469aeb5e6cf30fe0057934a39cdcf7e598ab0d6f
Denis Felst <denis.felst@phios.li>
2023-12-16 19:35:43 +0100

    treat sessionStorage cases for sending additionalInfo

--- src/services/additional-info-service.ts
+++ src/services/additional-info-service.ts
@@ -5,22 +5,81 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:8000"
 export const handleAdditionalInfoSubmission = async (
   additionalInfo: InfoObjectType[]
 ) => {
-  await Promise.all(
-    additionalInfo.map(async (item, index) => {
-      const variant_id = item.variant_id
-      const variant_title = item.variant_title
-      const letters = item.letters
+  let indexArr: string[] = []
+  let variantIdArr: string[] = []
+  let variantTitleArr: string[] = []
+  let letterArr: string[] = []
 
-      // submission to write on google spreadsheet
-      const response = await fetch(`${API_BASE_URL}/api/submission`, {
-        method: "POST",
-        // prettier-ignore
-        headers: {
+  const hasState = additionalInfo.length > 0
+
+  if (hasState) {
+    additionalInfo.map((item, index) => {
+      indexArr.push(index.toString())
+      variantIdArr.push(item.variant_id)
+      variantTitleArr.push(item.variant_title ?? "---")
+      letterArr.push(item.letters)
+    })
+    const index = indexArr.length
+    const variant_id = variantIdArr.join(" // ")
+    const variant_title = variantTitleArr.join(" // ")
+    const letters = letterArr.join(" // ")
+
+    // submission to write on google spreadsheet
+    const response = await fetch(`${API_BASE_URL}/api/submission`, {
+      method: "POST",
+      // prettier-ignore
+      headers: {
           "Accept": "application/json",
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ variant_id, variant_title, letters, index }),
-      })
+      body: JSON.stringify({
+        variant_id,
+        variant_title,
+        letters,
+        index,
+      }),
     })
-  )
+    return response
+  } else {
+    const arr = []
+
+    for (let i = 0; i < 50; i++) {
+      const value = sessionStorage.getItem(`item${i}`)
+      if (value) {
+        arr.push(value)
+      }
+    }
+
+    arr.forEach((str) => {
+      // Split the string into its components
+      const [variantd, title, letters] = str.split(" // ")
+
+      // Push the components into their respective arrays
+      variantIdArr.push(variantd)
+      variantTitleArr.push(title)
+      letterArr.push(letters)
+    })
+
+    const index = arr.length
+    const variant_id = variantIdArr.join(" // ")
+    const variant_title = variantTitleArr.join(" // ")
+    const letters = letterArr.join(" // ")
+
+    // submission to write on google spreadsheet
+    const response = await fetch(`${API_BASE_URL}/api/submission`, {
+      method: "POST",
+      // prettier-ignore
+      headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        variant_id,
+        variant_title,
+        letters,
+        index,
+      }),
+    })
+    return response
+  }
 }
