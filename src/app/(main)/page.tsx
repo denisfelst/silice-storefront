3316344a464b5da80acb3b160b027903a7c4966e
defelst <denis.felst@phios.li>
2024-03-20 19:24:53 +0100

    test deploy3

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,18 +1,7 @@
-import { getCollectionsList } from "@lib/data"
-import FeaturedProducts from "@modules/home/components/featured-products"
-import Hero from "@modules/home/components/hero"
-import SkeletonHomepageProducts from "@modules/skeletons/components/skeleton-homepage-products"
-import { Metadata } from "next"
-import { Suspense } from "react"
 
-export const metadata: Metadata = {
-  title: "Silice - Glass Keycaps",
-  description:
-    "Silice official online store for glass keycaps.",
-}
+
 
 export default async function Home() {
-  const { collections, count } = await getCollectionsList(0, 3)
 
   return (
     <>
@@ -20,4 +9,3 @@ export default async function Home() {
     </>
   )
 }
-// export const dynamic = 'force-static'
