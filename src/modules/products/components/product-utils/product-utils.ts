1ff4a1228648c4d9910e5228f1558b1c88503832
denisfelst <denis-fg@hotmail.com>
2024-06-09 13:19:12 +0200

    checkIfFilterOutValues
    // given an option, set which values will be displayed. If all of them, omit

---
+++ src/modules/products/components/product-utils/product-utils.ts
@@ -0,0 +1,18 @@
+import { MatchingRowsBySize, OptionsEnum, SizeValueEnum } from "@lib/constants"
+import { SelectOptions } from "../model/select-options"
+
+
+// given an option, set which values will be displayed. If all of them, omit  
+export const checkIfFilterOutValues = (title: string, originalSelectedOptions: SelectOptions, selectableOptions: string[]): any[] => {
+  if(title === OptionsEnum.Row){
+    switch(true){
+      case originalSelectedOptions.size === SizeValueEnum.Ctrl:
+        return MatchingRowsBySize.Ctrl           
+      case originalSelectedOptions.size === SizeValueEnum.Spacebar:
+        return MatchingRowsBySize.Spacebar 
+      case originalSelectedOptions.size === SizeValueEnum.Shift:
+        return MatchingRowsBySize.Shift 
+    }
+  }
+  return selectableOptions;
+}
