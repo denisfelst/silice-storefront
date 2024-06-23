a000578445bd325ae55ce8ef42478d2fcd50577b
denisfelst <denis-fg@hotmail.com>
2024-06-23 14:17:38 +0200

    landing page hero wip1

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -1,34 +1,45 @@
-import { Button, Heading } from "@medusajs/ui"
-import InteractiveLink from "@modules/common/components/interactive-link"
-import { Github } from "@medusajs/icons"
+'use client';
+
+import {  Heading } from "@medusajs/ui"
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+
 
 const Hero = () => {
+  const router = useRouter();
+
+  const handleButtonClick = () => {
+    // Navigate to another route
+    router.push('/products/classic');
+  };
+
+
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
-      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
-        <span>
-          <Heading
-            level="h1"
-            className="text-3xl leading-10 text-ui-fg-base font-normal"
-          >
-            Ecommerce Starter Template
-          </Heading>
-          <Heading
-            level="h2"
-            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
-          >
-            Powered by Medusa and Next.js
-          </Heading>
-        </span>
-        <a
-          href="https://github.com/medusajs/nextjs-starter-medusa"
-          target="_blank"
-        >
-          <Button variant="secondary">
-            View on GitHub
-            <Github />
-          </Button>
-        </a>
+    <div className="main-hero-container h-[70vh] w-full border-b border-ui-border-base bg-ui-bg-subtle bg-black relative lg:overflow-hidden">
+      <div className="absolute z-10 flex flex-col justify-end items-end text-center w-full h-full lg:overflow-hidden">
+        <Image src={"/hero.jpg"} alt="hero image" width={0} height={0} sizes="100vw" className="absolute inset-0 z-0" style={{ width: '100%', height: 'auto' }}/>
+        <div className="inside-hero relative z-10 border border-green-500 lg:border-red-500 w-full h-1/3 flex flex-col justify-start items-center lg:w-1/2 lg:h-1/2 ">
+          <span>
+            <Heading
+              level="h1"
+              className="text-3xl leading-10 text-ui-fg-base font-normal text-white mb-2"
+            >
+              Glass Crafted Keycaps
+            </Heading>
+            <Heading
+              level="h2"
+              className="text-xl leading-6 text-ui-fg-subtle font-normal text-gray-100 mb-4"
+              >
+              The First and Only Glass Keycaps in the Market. <br/>
+              Now at Your Fingertips.
+            </Heading>
+          </span>
+            <button onClick={handleButtonClick} className="relative flex flex-col items-start p-1 gap-2 w-36 h-14">
+              <div className="flex items-center justify-center p-4 gap-2 w-full h-full rounded-full bg-gradient-to-r from-red-500 to-red-500 hover:to-red-300">
+                <span className="font-bold text-white uppercase text-md">Shop Now</span>
+              </div>
+            </button>
+        </div>
       </div>
     </div>
   )
--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -20,7 +20,7 @@ const Nav = () => {
 
   return (
     <div className="sticky top-0 inset-x-0 z-50 group">
-      <header className="relative h-16 px-8 mx-auto border-b duration-200 bg-white border-ui-border-base">
+      <header className="relative h-12 px-8 mx-auto border-b duration-200 bg-white border-ui-border-base">
         <nav className="txt-xsmall-plus text-ui-fg-subtle flex items-center justify-between w-full h-full text-small-regular">
           <div className="flex-1 basis-0 h-full flex items-center">
             <div className="block small:hidden">
