822f4e408b9fc9b17f67e8912937a7bdee441f1c
denisfelst <denis-fg@hotmail.com>
2024-07-27 17:24:47 +0200

    add store preview 3

--- src/modules/home/components/store-preview/index.tsx
+++ src/modules/home/components/store-preview/index.tsx
@@ -17,17 +17,17 @@ const StorePreview = () => {
     <section className="main-store-preview-container max-w-screen-xl">
 
       <div className="relative min-h-[30vh] h-full max-h-[35vh] min-w-[400px] max-w-screen-xl 2xl:max-w-screen-2xl">
-        <div className="text-container absolute">
-          <Heading level="h2" className="font-normal text-white mb-2 text-lg md:text-md xl:text-lg 2xl:text-xl 2xl:mb-4">
+        <div className="text-container w-full h-full flex flex-col justify-center items-center absolute font-normal text-gray-800">
+          <Heading level="h2" className="mb-2 text-xl 2xl:text-2xl 2xl:mb-4">
             Check out our newest release
           </Heading>
-          <Heading level="h3">Our purest expression of our product vision</Heading>
-          <button className="relative bg-transparent text-white border-b-2 border-gray-800 py-2 px-4 transition-colors duration-300 ease-in-out hover:text-gray-600 hover:border-gray-600">
+          <Heading level="h3" className="text-md mb-4 md:text-sm xl:text-md 2xl:text-lg 2xl:mb-4">Our purest expression of our product vision</Heading>
+          <button className="relative bg-transparent text-red-500 font-bold border-b-2 border-gray-800 py-2 px-4 transition-colors duration-300 ease-in-out hover:text-yellow-500 hover:border-gray-600">
             Shop
           </button>
         </div>
         <div className="w-full overflow-hidden">
-          <Image className="w-full h-full object-cover" src={"/from_side.jpg"} quality="100" alt="store preview image" width={0} height={0} sizes="fill" style={{ width: '100%', height: 'auto', minHeight: '30vh' }} />
+          <Image draggable={false} className="w-full h-full object-cover" src={"/from_side_xl.jpg"} quality="100" alt="store preview image" width={0} height={0} sizes="fill" style={{ width: '100%', height: 'auto', minHeight: '30vh' }} />
         </div>
       </div>
 
