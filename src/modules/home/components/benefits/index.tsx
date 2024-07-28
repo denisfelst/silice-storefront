72b90b198a4669147ed47cada0e06f6cb5620f08
denisfelst <denis-fg@hotmail.com>
2024-07-28 18:13:26 +0200

    improve benefits

--- src/modules/home/components/benefits/index.tsx
+++ src/modules/home/components/benefits/index.tsx
@@ -2,63 +2,62 @@ import { Heading } from "@medusajs/ui"
 
 const Benefits = () => {
   return (
-    <section className="main-benefits-container text-white flex flex-col md:flex-row max-w-screen-xl">
-      <div className="h-[30vh] max-h-[370px] w-full bg-[#454855] transition-colors duration-300 ease-linear hover:bg-[#555865] md:h-[40vh] lg:max-h-none">
-        <div className="flex flex-col justify-center items-center text-center w-full h-full overflow-hidden p-5 text-white font-medium">
+    <section className="main-benefits-container text-white flex flex-col md:flex-row w-screen-xl max-w-screen-xl w-full">
+      <div className="w-full bg-[#454855] transition-colors duration-300 ease-linear hover:bg-[#555865] md:h-[40vh] lg:max-h-none">
+        <div className="flex flex-col justify-center items-center text-center w-full h-full overflow-hidden p-6 text-white font-medium">
           <Heading level="h2" className="mb-4 font-semibold text-2xl">
             Material
           </Heading>
           <span>
-            Crafted from premium glass, our keycaps offer unmatched clarity,
-            elegance, and durability, elevating your keyboard with a touch of
-            sophistication.
+            Crafted from premium glass, elevate your typing experience.
           </span>
-          <br />
-          <ul>
-            <li>Premium Glass Billets</li>
-            <li>High Durability & Resistance</li>
-            <li>Experience: Sight, Touch, Sound</li>
-            <li>Innovative Process & Creations</li>
-          </ul>
+          <div className="px-6 mt-3">
+            <ul className="list-disc text-left font-normal">
+              <li>Premium Glass Billets</li>
+              <li>High Durability & Resistance</li>
+              <li>Experience: Sight, Touch, Sound</li>
+              <li>Innovative Process & Creations</li>
+            </ul>
+          </div>
         </div>
       </div>
 
-      <div className="h-[30vh] max-h-[370px] w-full relative bg-[#737ca1] transition-colors duration-300 ease-linear hover:bg-[#838cb1] md:h-[40vh] lg:max-h-none">
-        <div className="flex flex-col justify-center items-center text-center w-full h-full overflow-hidden p-5 text-white font-medium">
+      <div className="w-full bg-[#737ca1] transition-colors duration-300 ease-linear hover:bg-[#838cb1] md:h-[40vh] lg:max-h-none">
+        <div className="flex flex-col justify-center items-center text-center w-full h-full overflow-hidden p-6 text-white font-medium">
           <Heading level="h2" className="mb-4 font-semibold text-2xl">
             Artisan Process
           </Heading>
           <span>
-            Handcrafted by skilled artisans, each keycap is a testament to
-            precision and quality, ensuring every piece is a unique work of art.
+            Handcrafted by skilled artisans, every piece is a unique work of
+            art.
           </span>
-          <br />
-          <ul>
-            <li>Moldmaking</li>
-            <li>Glass Casting & Kiln Forming</li>
-            <li>Sandering & Polishing </li>
-            <li>Engraving</li>
-          </ul>
+          <div className="px-6 mt-3">
+            <ul className="list-disc text-left font-normal">
+              <li>Moldmaking</li>
+              <li>Glass Casting & Kiln Forming</li>
+              <li>Sandering & Polishing </li>
+              <li>Engraving</li>
+            </ul>
+          </div>
         </div>
       </div>
 
-      <div className="h-[30vh] max-h-[370px] w-full relative bg-[#bdc4e0] transition-colors duration-300 ease-linear hover:bg-[#adb4d0] md:h-[40vh] lg:max-h-none">
-        <div className="flex flex-col justify-center items-center text-center w-full h-full overflow-hidden p-5 text-white font-medium">
+      <div className="w-full bg-[#bdc4e0] transition-colors duration-300 ease-linear hover:bg-[#adb4d0] md:h-[40vh] lg:max-h-none">
+        <div className="flex flex-col justify-center items-center text-center w-full h-full overflow-hidden p-6 text-white font-medium">
           <Heading level="h2" className="mb-4 font-semibold text-2xl">
             Exclusive & Custom
           </Heading>
           <span>
-            Enjoy customization with our glass keycaps, allowing you to choose
-            from different design and size combination for a unique keyboard
-            setup
+            Choose your design and size combination for a unique keyboard setup.
           </span>
-          <br />
-          <ul>
-            <li>Limited Time Frame Orders</li>
-            <li>100% On Demand: You Order, We Make it</li>
-            <li>Highly Customized keycaps</li>
-            <li>The Only Glass Keycaps Available Worldwide</li>
-          </ul>
+          <div className="px-6 mt-3">
+            <ul className="list-disc text-left font-normal">
+              <li>Unique in the Market</li>
+              <li>100% On Demand</li>
+              <li>Highly Customizable</li>
+              <li>Limited Time Frame Orders</li>
+            </ul>
+          </div>
         </div>
       </div>
     </section>
