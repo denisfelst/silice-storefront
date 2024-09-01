d5a86a7fb95811b60cd1134534aa6e35a5b86368
denisfelst <denis-fg@hotmail.com>
2024-09-01 14:19:26 +0200

    apply fav versions for 'benefits' and 'how it works'

--- src/modules/home/components/benefits/index.tsx
+++ src/modules/home/components/benefits/index.tsx
@@ -5,115 +5,79 @@ const Benefits = () => {
   return (
     <section className="main-benefits-container flex flex-col md:flex-row w-screen-xl max-w-screen-xl w-full p-6 space-y-6 md:space-y-0 md:space-x-6">
       {/* Benefit Card 1 */}
-      <div className="relative w-full bg-white rounded-lg overflow-hidden">
+      <div className="relative w-full bg-gray-100 rounded-lg overflow-hidden">
         <Image
           src="/glass1-small.jpg"
           alt="Glass texture"
           width={0}
           height={0}
           sizes="100vw"
-          className="absolute inset-0 w-full h-full object-cover opacity-50"
+          className="w-full h-72 object-cover"
         />
-        <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-transparent to-transparent opacity-70"></div>
-        <div className="relative flex flex-col justify-center items-center text-center w-full h-full p-6 text-gray-900 font-medium">
-          <Heading level="h2" className="mb-4 text-2xl font-semibold">
-            Material
-          </Heading>
-          <span className="mb-4">
-            Crafted from premium glass, elevate your typing experience.
-          </span>
-          <ul className="list-disc text-left space-y-2">
-            <li className="flex items-center">
-              <i className="fas fa-gem mr-2 text-lg"></i> Premium Glass Billets
-            </li>
-            <li className="flex items-center">
-              <i className="fas fa-shield-alt mr-2 text-lg"></i> High Durability
-              & Resistance
-            </li>
-            <li className="flex items-center">
-              <i className="fas fa-eye mr-2 text-lg"></i> Experience: Sight,
-              Touch, Sound
-            </li>
-            <li className="flex items-center">
-              <i className="fas fa-lightbulb mr-2 text-lg"></i> Innovative
-              Processes & Creations
-            </li>
-          </ul>
+        <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-white">
+          <div className="text-center p-6">
+            <Heading
+              level="h2"
+              className="mb-4 text-3xl font-semibold leading-none"
+            >
+              Material
+            </Heading>
+            <p className="text-lg">
+              Crafted from premium glass, elevate your typing experience.
+            </p>
+          </div>
         </div>
       </div>
 
       {/* Benefit Card 2 */}
-      <div className="relative w-full bg-white rounded-lg overflow-hidden">
+      <div className="relative w-full bg-gray-100 rounded-lg overflow-hidden">
         <Image
           src="/taller2.jpg"
           alt="Artisan process"
           width={0}
           height={0}
           sizes="100vw"
-          className="absolute inset-0 w-full h-full object-cover opacity-50"
+          className="w-full h-72 object-cover"
         />
-        <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-transparent to-transparent opacity-70"></div>
-        <div className="relative flex flex-col justify-center items-center text-center w-full h-full p-6 text-gray-900 font-medium">
-          <Heading level="h2" className="mb-4 text-2xl font-semibold">
-            Artisan Process
-          </Heading>
-          <span className="mb-4">
-            Handcrafted by skilled artisans, every piece is a unique work of
-            art.
-          </span>
-          <ul className="list-disc text-left space-y-2">
-            <li className="flex items-center">
-              <i className="fas fa-cube mr-2 text-lg"></i> Moldmaking
-            </li>
-            <li className="flex items-center">
-              <i className="fas fa-fire mr-2 text-lg"></i> Glass Casting & Kiln
-              Forming
-            </li>
-            <li className="flex items-center">
-              <i className="fas fa-hand-sparkles mr-2 text-lg"></i> Sandering &
-              Polishing
-            </li>
-            <li className="flex items-center">
-              <i className="fas fa-pen mr-2 text-lg"></i> Engraving
-            </li>
-          </ul>
+        <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-white">
+          <div className="text-center p-6">
+            <Heading
+              level="h2"
+              className="mb-4 text-3xl font-semibold leading-none"
+            >
+              Artisan Process
+            </Heading>
+            <p className="text-lg">
+              Handcrafted by skilled artisans, every piece is a unique work of
+              art.
+            </p>
+          </div>
         </div>
       </div>
 
       {/* Benefit Card 3 */}
-      <div className="relative w-full bg-white rounded-lg overflow-hidden">
+      <div className="relative w-full bg-gray-100 rounded-lg overflow-hidden">
         <Image
           src="/glass3.jpg"
           alt="Custom keyboard"
           width={0}
           height={0}
           sizes="100vw"
-          className="absolute inset-0 w-full h-full object-cover opacity-50"
+          className="w-full h-72 object-cover"
         />
-        <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-transparent to-transparent opacity-70"></div>
-        <div className="relative flex flex-col justify-center items-center text-center w-full h-full p-6 text-gray-900 font-medium">
-          <Heading level="h2" className="mb-4 text-2xl font-semibold">
-            Exclusive & Custom
-          </Heading>
-          <span className="mb-4">
-            Choose your design and size combination for a unique keyboard setup.
-          </span>
-          <ul className="list-disc text-left space-y-2">
-            <li className="flex items-center">
-              <i className="fas fa-gem mr-2 text-lg"></i> Unique in the Market
-            </li>
-            <li className="flex items-center">
-              <i className="fas fa-clock mr-2 text-lg"></i> 100% On Demand
-            </li>
-            <li className="flex items-center">
-              <i className="fas fa-sliders-h mr-2 text-lg"></i> Highly
-              Customizable
-            </li>
-            <li className="flex items-center">
-              <i className="fas fa-calendar-alt mr-2 text-lg"></i> Limited Time
-              Frame Orders
-            </li>
-          </ul>
+        <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center text-white">
+          <div className="text-center p-6">
+            <Heading
+              level="h2"
+              className="mb-4 text-3xl font-semibold leading-none"
+            >
+              Exclusive & Custom
+            </Heading>
+            <p className="text-lg">
+              Choose your design and size combination for a unique keyboard
+              setup.
+            </p>
+          </div>
         </div>
       </div>
     </section>
--- src/modules/home/components/how-it-works/index.tsx
+++ src/modules/home/components/how-it-works/index.tsx
@@ -10,39 +10,31 @@ const HowItWorks = () => {
         >
           How It Works
         </Heading>
-        <div className="relative">
-          <div className="absolute inset-0 flex flex-col items-center">
-            <div className="border-l-2 border-gray-300 absolute h-full left-1/2 transform -translate-x-1/2"></div>
+        <div className="flex flex-col space-y-8">
+          <div className="relative flex flex-col items-center text-center bg-white rounded-lg shadow-lg p-6">
+            <div className="w-12 h-12 bg-gray-400 text-white rounded-full flex items-center justify-center mb-4">
+              <span className="text-xl font-bold">1</span>
+            </div>
+            <span className="text-lg font-semibold text-gray-800">
+              Choose your favorite keycap(s).
+            </span>
           </div>
-          <div className="flex flex-col space-y-8">
-            <div className="relative flex flex-col items-center text-center bg-white rounded-lg shadow-md p-6">
-              <div className="w-12 h-12 bg-blue-500 text-white rounded-full flex items-center justify-center mb-4">
-                <i className="fas fa-keyboard text-2xl"></i>{" "}
-                {/* Font Awesome icon */}
-              </div>
-              <span className="text-lg font-semibold text-gray-800">
-                Choose your favorite keycap(s).
-              </span>
+          <div className="relative flex flex-col items-center text-center bg-white rounded-lg shadow-lg p-6">
+            <div className="w-12 h-12 bg-gray-500 text-white rounded-full flex items-center justify-center mb-4">
+              <span className="text-xl font-bold">2</span>
             </div>
-            <div className="relative flex flex-col items-center text-center bg-white rounded-lg shadow-md p-6">
-              <div className="w-12 h-12 bg-green-500 text-white rounded-full flex items-center justify-center mb-4">
-                <i className="fas fa-cogs text-2xl"></i>{" "}
-                {/* Font Awesome icon */}
-              </div>
-              <span className="text-lg font-semibold text-gray-800">
-                We get your order and start producing as soon as the order
-                period ends.
-              </span>
+            <span className="text-lg font-semibold text-gray-800">
+              We get your order and start producing as soon as the order period
+              ends.
+            </span>
+          </div>
+          <div className="relative flex flex-col items-center text-center bg-white rounded-lg shadow-lg p-6">
+            <div className="w-12 h-12 bg-gray-600 text-white rounded-full flex items-center justify-center mb-4">
+              <span className="text-xl font-bold">3</span>
             </div>
-            <div className="relative flex flex-col items-center text-center bg-white rounded-lg shadow-md p-6">
-              <div className="w-12 h-12 bg-red-500 text-white rounded-full flex items-center justify-center mb-4">
-                <i className="fas fa-truck text-2xl"></i>{" "}
-                {/* Font Awesome icon */}
-              </div>
-              <span className="text-lg font-semibold text-gray-800">
-                As soon as we’ve finished producing it, we’ll ship it to you.
-              </span>
-            </div>
+            <span className="text-lg font-semibold text-gray-800">
+              As soon as we’ve finished producing it, we’ll ship it to you.
+            </span>
           </div>
         </div>
       </div>
